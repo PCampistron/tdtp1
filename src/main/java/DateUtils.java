1413cd7a691dd8143129572c71819c73790cf25a
Pierre Campistron <92059222+PCampistron@users.noreply.github.com>
2024-02-06 12:27:47 +0100

    Correction du commit précédent

--- src/main/java/DateUtils.java
+++ src/main/java/DateUtils.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 public class DateUtils {
     public static boolean isNowBetween(LocalDate startingDate, LocalDate endingDate) {
-        return isDateBetween((startingDate, endingDate, LocalDate.now(), true);
+        return isDateBetween(startingDate, endingDate, LocalDate.now(), true);
     }
 
     static boolean isDateBetween(LocalDate startingDate, LocalDate endingDate, LocalDate date, boolean inclusive) {
